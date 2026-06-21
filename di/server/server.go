f04f46e1b274dcb7d87f19641ffbc08309d34e01
thanutgit <thanutsukprasertsomm@hotmail.com>
2026-06-21 13:32:44 +0700

    ai

--- di/server/server.go
+++ di/server/server.go
@@ -7,14 +7,12 @@ import (
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/logger"
-	"github.com/gofiber/fiber/v3/middleware/recover"
 	"gorm.io/gorm"
 )
 
 func InitApiServer(db *gorm.DB) error {
 	app := fiber.New()
-	app.Use(recover.New()) // กัน handler panic ทำทั้ง server ตาย
-	app.Use(logger.New())  // log ทุก request
+	app.Use(logger.New()) // log ทุก request
 	cfg := config.GetConfig()
 
 	router.InitRouter(app, db)
